a8abd3df8e674b8d30455f8e56f2c7fd903599bc
dennyphilippgoncalves <denny.philipp@outlook.com>
2023-05-25 17:25:49 -0300

    Initial

---
+++ src/main/java/com/example/exampleasync/ExampleAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exampleasync/ExampleAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
